26c8965a5633505503bfc1486df44e2c0b520b16
Payal91799 <payal.singh@teamhgs.com>
2025-05-07 14:55:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The current movie title is',name_on_order)
 
-"""cnx = st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
@@ -46,8 +46,8 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                #values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     
     #st.write(my_insert_stmt)
     
@@ -63,4 +63,4 @@ if ingredients_list:
     ingredients_string +=  fruit_chosen + ' '
     st.subheader(fruit_chosen + ' Nutrition Information')
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+ fruit_chosen)
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)"""
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
